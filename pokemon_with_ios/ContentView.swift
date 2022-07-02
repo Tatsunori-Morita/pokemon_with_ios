69a041da23440d5eb3078f60456900d8de11a528
Tatsunori Morita <tatsunori.morita@gmail.com>
2022-07-02 22:02:49 +0900

    first commit.

---
+++ pokemon_with_ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  pokemon_with_ios
+//
+//  Created by Tatsunori on 2022/07/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ pokemon_with_ios/pokemon_with_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokemon_with_iosApp.swift
+//  pokemon_with_ios
+//
+//  Created by Tatsunori on 2022/07/02.
+//
+
+import SwiftUI
+
+@main
+struct pokemon_with_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

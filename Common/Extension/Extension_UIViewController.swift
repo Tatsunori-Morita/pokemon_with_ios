7fa1262e313ef1b1132c41b9ad0375b89489062b
Tatsunori-Morita <tatsunori.morita@gmail.com>
2023-06-29 17:34:17 +0900

    UIViewControllerのExtensionを作成

---
+++ Common/Extension/Extension_UIViewController.swift
@@ -0,0 +1,41 @@
+//
+//  Extension_UIViewController.swift
+//  pokemon_with_ios
+//
+//  Created by Tatsunori on 2023/06/29.
+//
+
+import SwiftUI
+
+struct ViewControllerHolder {
+    weak var value: UIViewController?
+}
+
+struct ViewControllerKey: EnvironmentKey {
+    static var defaultValue: ViewControllerHolder {
+        let scenes = UIApplication.shared.connectedScenes
+        let windowScenes = scenes.first as? UIWindowScene
+        return ViewControllerHolder(value: windowScenes?.keyWindow?.rootViewController)
+    }
+}
+
+extension EnvironmentValues {
+    var viewController: UIViewController? {
+        get { return self[ViewControllerKey.self].value }
+        set { self[ViewControllerKey.self].value = newValue }
+    }
+}
+
+extension UIViewController {
+    func present<Content: View>(style: UIModalPresentationStyle = .automatic, transitionStyle: UIModalTransitionStyle = .coverVertical, @ViewBuilder builder: () -> Content) {
+        let toPresent = UIHostingController(rootView: AnyView(EmptyView()))
+        toPresent.modalPresentationStyle = style
+        toPresent.modalTransitionStyle = transitionStyle
+        toPresent.view.backgroundColor = UIColor.init(red: 0, green: 0, blue: 0, alpha: 0.5)
+        toPresent.rootView = AnyView(
+            builder()
+                .environment(\.viewController, toPresent)
+        )
+        self.present(toPresent, animated: true, completion: nil)
+    }
+}
--- pokemon_with_ios/View/ModalPopUpView.swift
+++ pokemon_with_ios/View/ModalPopUpView.swift
@@ -7,39 +7,6 @@
 
 import SwiftUI
 
-struct ViewControllerHolder {
-    weak var value: UIViewController?
-}
-
-struct ViewControllerKey: EnvironmentKey {
-    static var defaultValue: ViewControllerHolder {
-        let scenes = UIApplication.shared.connectedScenes
-        let windowScenes = scenes.first as? UIWindowScene
-        return ViewControllerHolder(value: windowScenes?.keyWindow?.rootViewController)
-    }
-}
-
-extension EnvironmentValues {
-    var viewController: UIViewController? {
-        get { return self[ViewControllerKey.self].value }
-        set { self[ViewControllerKey.self].value = newValue }
-    }
-}
-
-extension UIViewController {
-    func present<Content: View>(style: UIModalPresentationStyle = .automatic, transitionStyle: UIModalTransitionStyle = .coverVertical, @ViewBuilder builder: () -> Content) {
-        let toPresent = UIHostingController(rootView: AnyView(EmptyView()))
-        toPresent.modalPresentationStyle = style
-        toPresent.modalTransitionStyle = transitionStyle
-        toPresent.view.backgroundColor = UIColor.init(red: 0, green: 0, blue: 0, alpha: 0.5)
-        toPresent.rootView = AnyView(
-            builder()
-                .environment(\.viewController, toPresent)
-        )
-        self.present(toPresent, animated: true, completion: nil)
-    }
-}
-
 struct ModalPopUpView: View {
     @Environment(\.viewController) private var viewControllerHolder: UIViewController?
 

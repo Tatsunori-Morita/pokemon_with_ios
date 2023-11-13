023c5804b33bdfbb930b8c8b248d4e6598f86f3e
Tatsunori-Morita <tatsunori.morita@gmail.com>
2023-11-13 15:09:30 +0900

    Constクラスと別ファイルに変更

--- Common/Domains/Config/DomainConfig.swift
+++ Common/Domains/Config/DomainConfig.swift
@@ -52,23 +52,3 @@ class LibraryContentViewConfig: SystemConfig {
         _additionalNumberOfPokemon
     }
 }
-
-struct Const {
-    public static let shared = Const()
-    
-    public var getPokemonAmount: Int {
-        1010
-    }
-    
-    public var getPokemonUrl: String {
-        "https://pokeapi.co/api/v2/pokemon/"
-    }
-    
-    public var getPokemonSpeciesUrl: String {
-        "https://pokeapi.co/api/v2/pokemon-species/"
-    }
-    
-    public var getPokemonFrontDefault: String {
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/%@.png"
-    }
-}
